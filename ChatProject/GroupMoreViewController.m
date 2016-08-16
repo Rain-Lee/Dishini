9366e53fab620d3c267e554a3aa8f2f1550652aa
Rain-Li <lfqyusw@gmail.com>
2016-08-16 14:59:26 +0800

    no message

--- ChatProject/GroupMoreViewController.m
+++ ChatProject/GroupMoreViewController.m
@@ -136,6 +136,122 @@
     }
 }
 
+#pragma mark UICollectionViewDelegate
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 6;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    if (section == 1 || section == 2 || section == 3 || section == 4 || section == 5) {
+        return 1;
+    }else{
+        return groupMemberData.count + (isManager ? 2 : 1);
+    }
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        GroupMoreCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+        @try {
+            if (isManager && indexPath.item >= groupMemberData.count){
+                if (indexPath.row == groupMemberData.count) {
+                    cell.photoIv.image = [UIImage imageNamed:@"jia"];
+                    cell.nameLbl.text = @"";
+                }else{
+                    cell.photoIv.image = [UIImage imageNamed:@"jian"];
+                    cell.nameLbl.text = @"";
+                }
+            }else if (!isManager && indexPath.item == groupMemberData.count){
+                cell.photoIv.image = [UIImage imageNamed:@"jia"];
+                cell.nameLbl.text = @"";
+            }else{
+                NSString *photoPath = [NSString stringWithFormat:@"%@%@",Kimg_path,groupMemberData[indexPath.item][@"PhotoPath"]];
+                [cell.photoIv sd_setImageWithURL:[NSURL URLWithString:photoPath] placeholderImage:[UIImage imageNamed:@"default_photo"]];
+                cell.photoIv.layer.masksToBounds = true;
+                cell.photoIv.layer.cornerRadius = 6;
+                cell.nameLbl.text = [groupMemberData[indexPath.item][@"RemarkName"] isEqual:@""] ? groupMemberData[indexPath.item][@"NicName"] : groupMemberData[indexPath.item][@"RemarkName"];
+            }
+        }
+        @catch (NSException *exception) {
+            
+        }
+        @finally {
+            return cell;
+        }
+    }else{
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifierTwo forIndexPath:indexPath];
+        for (UIView *itemView in cell.contentView.subviews) {
+            [itemView removeFromSuperview];
+        }
+        cell.backgroundColor = [UIColor whiteColor];
+        if (indexPath.section == 1) {
+            // lineView
+            UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(15, 0, SCREEN_WIDTH - 15, 1)];
+            lineView.backgroundColor = [UIColor colorWithRed:0.90 green:0.90 blue:0.90 alpha:1.00];
+            [cell.contentView addSubview:lineView];
+            // titleLbl
+            UILabel *titleLbl = [[UILabel alloc] initWithFrame:CGRectMake(15, 1, SCREEN_WIDTH - 15, cell.frame.size.height - 0.5)];
+            titleLbl.text = [NSString stringWithFormat:@"全部群成员(%lu)",(unsigned long)groupMemberData.count];
+            titleLbl.font = [UIFont systemFontOfSize:15];
+            [cell.contentView addSubview:titleLbl];
+        }else if (indexPath.section == 2){
+            cell.backgroundColor = [UIColor colorWithRed:0.96 green:0.96 blue:0.96 alpha:1.00];
+        }else if (indexPath.section == 3){
+            // titleLbl
+            UILabel *titleLbl = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, SCREEN_WIDTH - 15, cell.frame.size.height)];
+            titleLbl.text = [NSString stringWithFormat:@"群名称"];
+            titleLbl.font = [UIFont systemFontOfSize:15];
+            [cell.contentView addSubview:titleLbl];
+            // nameLbl
+            UILabel *nameLbl = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 15 - 250, 0, 250, cell.frame.size.height)];
+            nameLbl.font = [UIFont systemFontOfSize:15];
+            nameLbl.textAlignment = NSTextAlignmentRight;
+            nameLbl.text = _groupName;
+            [cell.contentView addSubview:nameLbl];
+        }else if (indexPath.section == 4){
+            cell.backgroundColor = [UIColor colorWithRed:0.96 green:0.96 blue:0.96 alpha:1.00];
+        }else{
+            UIButton *_quitBtn = [[UIButton alloc]init];
+            _quitBtn.frame = CGRectMake(15, 15, cell.frame.size.width - 30, cell.frame.size.height);
+            _quitBtn.backgroundColor = [UIColor colorWithRed:0.94 green:0.00 blue:0.03 alpha:1.00];
+            [_quitBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            _quitBtn.layer.masksToBounds = true;
+            _quitBtn.layer.cornerRadius = 6;
+            if (isManager) {
+                [_quitBtn setTitle:@"退出并删除" forState:UIControlStateNormal];
+                _quitBtn.tag = 1;
+                [_quitBtn addTarget:self action:@selector(quitBtn:) forControlEvents:UIControlEventTouchUpInside];
+            }else{
+                [_quitBtn setTitle:@"退出" forState:UIControlStateNormal];
+                _quitBtn.tag = 2;
+                [_quitBtn addTarget:self action:@selector(quitBtn:) forControlEvents:UIControlEventTouchUpInside];
+            }
+            [cell.contentView addSubview:_quitBtn];
+        }
+        return cell;
+    }
+    
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        return CGSizeMake((SCREEN_WIDTH - 5 * 10) / 4, (SCREEN_WIDTH - 5 * 10) / 4 + 20);
+    }else if (indexPath.section == 1 || indexPath.section == 3 || indexPath.section == 5){
+        return CGSizeMake(SCREEN_WIDTH, 50);
+    }else{
+        return CGSizeMake(SCREEN_WIDTH, 12);
+    }
+}
+
+//设置每个item的UIEdgeInsets
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    if (section == 0) {
+        return UIEdgeInsetsMake(10, 10, 10, 10);
+    }else{
+        return UIEdgeInsetsMake(0, 0, 0, 0);
+    }
+}
+
 
 
 //设置头尾的size
